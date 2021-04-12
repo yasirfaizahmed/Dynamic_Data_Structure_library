b1455c64903a64a417abaa720aa5fd46bfa23b83
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-04-12 21:26:01 +0530

    completed SLL testing

--- 3_Implementation/test/dyn_ds_l_test.c
+++ 3_Implementation/test/dyn_ds_l_test.c
@@ -17,6 +17,9 @@ int actual_len = 0;
 int data = 0;	//just a test data, increment this everytime when it gets inserted into SLL
 int pos = 0;	//just a testing poition value to work with SLL
 
+q_node* front_ = NULL;
+q_node* rear_ = NULL;
+
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -24,8 +27,8 @@ void setUp(){}
 void tearDown(){}
 
 
-/************************* SLL test functions prototyping **************************/
-/****************** Basic functions test ********************************/
+/************************* test functions prototyping **************************/
+/****************** SLL test functions ********************************/
 void test_sll_append( void );	
 void test_sll_len( void );
 void test_sll_add_at_begining( void );	
@@ -36,8 +39,17 @@ void test_sll_swap_data( void );
 void test_sll_reverse_list( void );
 void test_sll_peek( void );
 void test_sll_overwrite( void );
+void test_sll_sort_bubble( void );
+
+/****************** Queue test functions ********************************/
+void test_q_push( void );	
+void test_q_print_all( void );	
+void test_q_len( void );	
+void test_q_pop( void );
+void test_q_peek( void );	
 
 /***************** test functions definitions **************************************/
+/******************* SLL test function definitions**************/
 void test_sll_append( void ){	//appending 10 values to empty SLL
 	TEST_ASSERT_EQUAL(1, sll_append(&root, ++data));	//appending values to SLL, if sucess, it returns 1 else -1
 	actual_len++;				//incrementing the actual_len to test the test_sll_len later if needed
@@ -108,10 +120,31 @@ void test_sll_overwrite( void ){
 	TEST_ASSERT_EQUAL(1, sll_overwrite(&root, ++pos, 69));sll_print_all(root);
 }
 
+void test_sll_sort_bubble( void ){
+	TEST_ASSERT_EQUAL(1, sll_sort_bubble(&root, 'a'));sll_print_all(root);
+	TEST_ASSERT_EQUAL(1, sll_sort_bubble(&root, 'd'));sll_print_all(root);
+}
+
+
+/******************* Queue test function definitions**************/
+void test_q_push( void ){
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+	TEST_ASSERT_EQUAL(1, q_push(&front_, &rear_, ++data));q_print_all(front_);
+}
+
 int main(){
 	UNITY_BEGIN();	//initilize the uinity framework
 	
 	//RUN_TEST macro needs function pointer, in our case for eg. test_sll_append (function name is also a function pointer, hehehe)
+	/////// SLL testing
 	RUN_TEST(test_sll_append);	
 	printf("\n--------------------------------------------------------------------------\n");
 	
@@ -144,6 +177,19 @@ int main(){
 	
 	RUN_TEST(test_sll_overwrite);
 	printf("\n--------------------------------------------------------------------------\n");
+	
+	RUN_TEST(test_sll_sort_bubble);
+	printf("\n--------------------------------------------------------------------------\n");
+	printf("\n--------------------------------------------------------------------------\n");
+	printf("\n--------------------------------------------------------------------------\n");
+	pos = 0;actual_len = 0;data = 0;
+	
+	////// Queue testing
+	RUN_TEST(test_q_push);
+	printf("\n--------------------------------------------------------------------------\n");
+	
+	
+	
 	
 	return UNITY_END();
 	
